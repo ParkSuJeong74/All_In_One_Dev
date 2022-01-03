19422ffdd02b8bf6246c491ffb222d125f9575c9
ParkSuJeong <evergreen9158@naver.com>
2022-01-03 14:24:33 +0900

    react+axios, react-dom

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import MainPageComponent from './main';
+import "./App.css"
+import {Switch, Route} from "react-router-dom";
+import UploadPage from './upload';
+import ProductPage from './product';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent/>
+        </Route>
+        <Route exact={true} path="/product/:id">
+          <ProductPage/>
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage/>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+export default App;
